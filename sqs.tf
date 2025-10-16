aa63cd74a3c45d10e9c06249a8a0af8b5d89c8f7
Hubert Dumas <hubertd@gmail.com>
2025-10-16 16:49:27 +1100

    Done

--- sqs.tf
+++ sqs.tf
@@ -3,264 +3,96 @@
 # ============================================================================
 
 # Order Submitted Queues
-resource "aws_sqs_queue" "order_submitted_inventory_dlq" {
-  name                        = "order-submitted-inventory-dlq.fifo"
-  fifo_queue                  = true
-  content_based_deduplication = true
-  message_retention_seconds   = 1209600 # 14 days
-}
-
 resource "aws_sqs_queue" "order_submitted_inventory" {
   name                        = "order-submitted-inventory.fifo"
   fifo_queue                  = true
   content_based_deduplication = true
-
-  redrive_policy = jsonencode({
-    deadLetterTargetArn = aws_sqs_queue.order_submitted_inventory_dlq.arn
-    maxReceiveCount     = 3
-  })
-}
-
-resource "aws_sqs_queue" "order_submitted_fulfillment_dlq" {
-  name                        = "order-submitted-fulfillment-dlq.fifo"
-  fifo_queue                  = true
-  content_based_deduplication = true
-  message_retention_seconds   = 1209600 # 14 days
 }
 
 resource "aws_sqs_queue" "order_submitted_fulfillment" {
   name                        = "order-submitted-fulfillment.fifo"
   fifo_queue                  = true
   content_based_deduplication = true
-
-  redrive_policy = jsonencode({
-    deadLetterTargetArn = aws_sqs_queue.order_submitted_fulfillment_dlq.arn
-    maxReceiveCount     = 3
-  })
 }
 
 # Inventory Level Changed Queue
-resource "aws_sqs_queue" "inventory_level_changed_catalog_dlq" {
-  name                        = "inventory-level-changed-catalog-dlq.fifo"
-  fifo_queue                  = true
-  content_based_deduplication = true
-  message_retention_seconds   = 1209600 # 14 days
-}
-
 resource "aws_sqs_queue" "inventory_level_changed_catalog" {
   name                        = "inventory-level-changed-catalog.fifo"
   fifo_queue                  = true
   content_based_deduplication = true
-
-  redrive_policy = jsonencode({
-    deadLetterTargetArn = aws_sqs_queue.inventory_level_changed_catalog_dlq.arn
-    maxReceiveCount     = 3
-  })
 }
 
 # Inventory Reserved Queues
-resource "aws_sqs_queue" "inventory_reserved_order_dlq" {
-  name                        = "inventory-reserved-order-dlq.fifo"
-  fifo_queue                  = true
-  content_based_deduplication = true
-  message_retention_seconds   = 1209600 # 14 days
-}
-
 resource "aws_sqs_queue" "inventory_reserved_order" {
   name                        = "inventory-reserved-order.fifo"
   fifo_queue                  = true
   content_based_deduplication = true
-
-  redrive_policy = jsonencode({
-    deadLetterTargetArn = aws_sqs_queue.inventory_reserved_order_dlq.arn
-    maxReceiveCount     = 3
-  })
-}
-
-resource "aws_sqs_queue" "inventory_reserved_fulfillment_dlq" {
-  name                        = "inventory-reserved-fulfillment-dlq.fifo"
-  fifo_queue                  = true
-  content_based_deduplication = true
-  message_retention_seconds   = 1209600 # 14 days
 }
 
 resource "aws_sqs_queue" "inventory_reserved_fulfillment" {
   name                        = "inventory-reserved-fulfillment.fifo"
   fifo_queue                  = true
   content_based_deduplication = true
-
-  redrive_policy = jsonencode({
-    deadLetterTargetArn = aws_sqs_queue.inventory_reserved_fulfillment_dlq.arn
-    maxReceiveCount     = 3
-  })
 }
 
 # Order Stock Failed Queue
-resource "aws_sqs_queue" "order_stock_failed_order_dlq" {
-  name                        = "order-stock-failed-order-dlq.fifo"
-  fifo_queue                  = true
-  content_based_deduplication = true
-  message_retention_seconds   = 1209600 # 14 days
-}
-
 resource "aws_sqs_queue" "order_stock_failed_order" {
   name                        = "order-stock-failed-order.fifo"
   fifo_queue                  = true
   content_based_deduplication = true
-
-  redrive_policy = jsonencode({
-    deadLetterTargetArn = aws_sqs_queue.order_stock_failed_order_dlq.arn
-    maxReceiveCount     = 3
-  })
 }
 
 # Fulfillment Task Created Queue
-resource "aws_sqs_queue" "fulfillment_task_created_warehouse_dlq" {
-  name                        = "fulfillment-task-created-warehouse-dlq.fifo"
-  fifo_queue                  = true
-  content_based_deduplication = true
-  message_retention_seconds   = 1209600 # 14 days
-}
-
 resource "aws_sqs_queue" "fulfillment_task_created_warehouse" {
   name                        = "fulfillment-task-created-warehouse.fifo"
   fifo_queue                  = true
   content_based_deduplication = true
-
-  redrive_policy = jsonencode({
-    deadLetterTargetArn = aws_sqs_queue.fulfillment_task_created_warehouse_dlq.arn
-    maxReceiveCount     = 3
-  })
 }
 
 # Label Generated Queues
-resource "aws_sqs_queue" "shipping_labelgenerated_order_dlq" {
-  name                        = "shipping-labelgenerated-order-dlq.fifo"
-  fifo_queue                  = true
-  content_based_deduplication = true
-  message_retention_seconds   = 1209600 # 14 days
-}
-
 resource "aws_sqs_queue" "shipping_labelgenerated_order" {
   name                        = "shipping-labelgenerated-order.fifo"
   fifo_queue                  = true
   content_based_deduplication = true
-
-  redrive_policy = jsonencode({
-    deadLetterTargetArn = aws_sqs_queue.shipping_labelgenerated_order_dlq.arn
-    maxReceiveCount     = 3
-  })
-}
-
-resource "aws_sqs_queue" "shipping_labelgenerated_fulfillment_dlq" {
-  name                        = "shipping-labelgenerated-fulfillment-dlq.fifo"
-  fifo_queue                  = true
-  content_based_deduplication = true
-  message_retention_seconds   = 1209600 # 14 days
 }
 
 resource "aws_sqs_queue" "shipping_labelgenerated_fulfillment" {
   name                        = "shipping-labelgenerated-fulfillment.fifo"
   fifo_queue                  = true
   content_based_deduplication = true
-
-  redrive_policy = jsonencode({
-    deadLetterTargetArn = aws_sqs_queue.shipping_labelgenerated_fulfillment_dlq.arn
-    maxReceiveCount     = 3
-  })
 }
 
 # Warehouse Job Completed Queues
-resource "aws_sqs_queue" "job_completed_order_dlq" {
-  name                        = "job-completed-order-dlq.fifo"
-  fifo_queue                  = true
-  content_based_deduplication = true
-  message_retention_seconds   = 1209600 # 14 days
-}
-
 resource "aws_sqs_queue" "job_completed_order" {
   name                        = "job-completed-order.fifo"
   fifo_queue                  = true
   content_based_deduplication = true
-
-  redrive_policy = jsonencode({
-    deadLetterTargetArn = aws_sqs_queue.job_completed_order_dlq.arn
-    maxReceiveCount     = 3
-  })
-}
-
-resource "aws_sqs_queue" "job_completed_fulfillment_dlq" {
-  name                        = "job-completed-fulfillment-dlq.fifo"
-  fifo_queue                  = true
-  content_based_deduplication = true
-  message_retention_seconds   = 1209600 # 14 days
 }
 
 resource "aws_sqs_queue" "job_completed_fulfillment" {
   name                        = "job-completed-fulfillment.fifo"
   fifo_queue                  = true
   content_based_deduplication = true
-
-  redrive_policy = jsonencode({
-    deadLetterTargetArn = aws_sqs_queue.job_completed_fulfillment_dlq.arn
-    maxReceiveCount     = 3
-  })
 }
 
 # Job Pick In Progress Queues
-resource "aws_sqs_queue" "job_pickinprogress_order_dlq" {
-  name                        = "job-pickinprogress-order-dlq.fifo"
-  fifo_queue                  = true
-  content_based_deduplication = true
-  message_retention_seconds   = 1209600 # 14 days
-}
-
 resource "aws_sqs_queue" "job_pickinprogress_order" {
   name                        = "job-pickinprogress-order.fifo"
   fifo_queue                  = true
   content_based_deduplication = true
-
-  redrive_policy = jsonencode({
-    deadLetterTargetArn = aws_sqs_queue.job_pickinprogress_order_dlq.arn
-    maxReceiveCount     = 3
-  })
-}
-
-resource "aws_sqs_queue" "job_pickinprogress_fulfillment_dlq" {
-  name                        = "job-pickinprogress-fulfillment-dlq.fifo"
-  fifo_queue                  = true
-  content_based_deduplication = true
-  message_retention_seconds   = 1209600 # 14 days
 }
 
 resource "aws_sqs_queue" "job_pickinprogress_fulfillment" {
   name                        = "job-pickinprogress-fulfillment.fifo"
   fifo_queue                  = true
   content_based_deduplication = true
-
-  redrive_policy = jsonencode({
-    deadLetterTargetArn = aws_sqs_queue.job_pickinprogress_fulfillment_dlq.arn
-    maxReceiveCount     = 3
-  })
 }
 
 # Order Shipped Queue
-resource "aws_sqs_queue" "fulfillment_ordershipped_order_dlq" {
-  name                        = "fulfillment-ordershipped-order-dlq.fifo"
-  fifo_queue                  = true
-  content_based_deduplication = true
-  message_retention_seconds   = 1209600 # 14 days
-}
-
 resource "aws_sqs_queue" "fulfillment_ordershipped_order" {
   name                        = "fulfillment-ordershipped-order.fifo"
   fifo_queue                  = true
   content_based_deduplication = true
-
-  redrive_policy = jsonencode({
-    deadLetterTargetArn = aws_sqs_queue.fulfillment_ordershipped_order_dlq.arn
-    maxReceiveCount     = 3
-  })
 }
 
 # ============================================================================
@@ -490,6 +322,3 @@ resource "aws_sqs_queue_policy" "fulfillment_ordershipped_order" {
     }]
   })
 }
-
-# TODO: Set up CloudWatch alarms to monitor DLQ message counts
-# TODO: Create procedures for handling failed messages
